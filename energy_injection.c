0e9efe6a6b21787101b3ad40af0c2327f6ba836e
Nanoom Lee <nanoomlee@MacBook-Pro-2.local>
2020-08-23 21:23:44 -0400

    Removed typo

--- energy_injection.c
+++ energy_injection.c
@@ -1,6 +1,6 @@
 /******************************************************************************************************/
 /*                           HYREC-2: Hydrogen and Helium Recombination Code                          */
-/*                      Written by Yacine Ali-Haimoud and Chris Hirata (C2010-17)                     */
+/*                      Written by Yacine Ali-Haimoud and Chris Hirata (2010-17)                      */
 /*                            with contributions from Nanoom Lee (2020)                               */
 /*                                                                                                    */
 /*     energy_injection.c: functions for the energy injection rate by various physical processes      */
